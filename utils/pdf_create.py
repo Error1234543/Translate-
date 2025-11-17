aa241d5a8e502e6c33089a133d02129512a241b2
Error1234543 <ptaviyad21@gmail.com>
2025-11-17 16:11:22 +0530

    Update pdf_create.py

--- utils/pdf_create.py
+++ utils/pdf_create.py
@@ -1,9 +1,14 @@
 from fpdf import FPDF
 
-def create_pdf(text, output_path):
+def create_pdf_from_text(text, output_path):
+    """
+    Create PDF from text.
+    Each line becomes a new paragraph.
+    """
     pdf = FPDF()
     pdf.add_page()
     pdf.set_auto_page_break(auto=True, margin=15)
     pdf.set_font("Arial", size=12)
-    pdf.multi_cell(0, 8, text)
+    for line in text.split('\n'):
+        pdf.multi_cell(0, 8, line)
     pdf.output(output_path)
